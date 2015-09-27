efaf920fd157b17de3344595c9dc3f0b0a04a73e
Stepan <cntgfy123@gmail.com>
2015-09-28 02:16:21 +0300

    Sets and lists

--- Examples/structs/src/main/java/ru/mirea/oop/practice/lists/Algorithms.java
+++ Examples/structs/src/main/java/ru/mirea/oop/practice/lists/Algorithms.java
@@ -1,17 +1,69 @@
 package ru.mirea.oop.practice.lists;
 
+import java.util.Iterator;
+
 public final class Algorithms {
-    public static <E> void sort(ILinkedList<E> list) {
-        throw new RuntimeException("Not implement yet");
+    public static <E extends Comparable<E>> void sort(ILinkedList<E> list) {
+
+        E obj1 = null;
+        while (true) {
+            int k = 0;
+            Iterator<E> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                E obj = obj1;
+                obj1 = iterator.next();
+                int i = 0;
+                if (obj != null) {
+                    i = obj1.compareTo(obj);
+                }
+                if (i == -1) {
+                    list.remove(obj);
+                    list.add(obj);
+                    k++;
+                    obj1 = null;
+                    break;
+                }
+            }
+            if (k == 0) {
+                break;
+            }
+        }
     }
 
     public static <E> ILinkedList<E> subList(ILinkedList<E> list, E offset) {
-        throw new RuntimeException("Not implement yet");
+
+        ILinkedList<E> linkedList = new LinkedListImpl<E>();
+        Iterator<E> iterator = list.iterator();
+        int k = 0;
+        while (iterator.hasNext()) {
+            E obj = iterator.next();
+            if (obj.equals(offset) || k != 0) {
+
+                linkedList.add(obj);
+                k++;
+            }
+        }
+
+        return linkedList;
     }
 
+
     public static <E> ILinkedList<E> subList(ILinkedList<E> list, E offset, int size) {
-        throw new RuntimeException("Not implement yet");
+        ILinkedList<E> linkedList = new LinkedListImpl<E>();
+        Iterator<E> iterator = list.iterator();
+        int k = 0;
+        while (iterator.hasNext()) {
+            E obj = iterator.next();
+            if (obj.equals(offset) || (k != 0 && k < size)) {
+
+                linkedList.add(obj);
+                k++;
+            }
+        }
+
+        return linkedList;
     }
+
 
     public static <E> ILinkedList<E> addList(ILinkedList<E> list, ILinkedList<E> other) {
         for (E elem : other)
@@ -20,7 +72,19 @@ public final class Algorithms {
     }
 
     public static <E> ILinkedList<E> removeList(ILinkedList<E> list, E offset, int size) {
-        throw new RuntimeException("Not implement yet");
+
+        Iterator<E> iterator = list.iterator();
+        int k = 0;
+        while (iterator.hasNext()) {
+            E obj = iterator.next();
+            if (obj.equals(offset) || (k != 0 && k < size)) {
+                list.remove(obj);
+                k++;
+
+            }
+        }
+        return list;
+
     }
 
     public static <E> boolean compareList(ILinkedList<E> list, ILinkedList<E> other) {
@@ -28,6 +92,7 @@ public final class Algorithms {
             return false;
         return false;
     }
+
 
     public interface LinkedListFold<R, E> {
         R execute(R accum, E element);
@@ -39,4 +104,5 @@ public final class Algorithms {
         }
         return accum;
     }
+
 }
