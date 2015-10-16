eabc1c33af98e5a45bcb6282914d66c8dd08e5f5
Андрей <viruszold@mail.ru>
2015-10-16 11:21:19 +0300

    Начинаем проверять

--- Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/AccessTokenAuthenticator.java
+++ Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/AccessTokenAuthenticator.java
@@ -6,10 +6,15 @@ import com.squareup.okhttp.Request;
 
 final class AccessTokenAuthenticator {
 
-    public static void setAccessToken(OkHttpClient ok, String accessToken) {
+    private AccessTokenAuthenticator() {
+    }
+
+    public static void setAccessToken(final OkHttpClient ok,
+                                      final String accessToken) {
         ok.networkInterceptors().add(chain -> {
             Request request = chain.request();
-            HttpUrl httpUrl = request.httpUrl().newBuilder().addQueryParameter("access_token", accessToken).build();
+            HttpUrl httpUrl = request.httpUrl().newBuilder()
+                    .addQueryParameter("access_token", accessToken).build();
             //NOTIFY: Для отладки можно включить
             //System.out.println("Request: " + httpUrl.toString());
             return chain.proceed(request.newBuilder().url(httpUrl).build());
