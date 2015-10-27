a7d8b3874876b7e8d77f80e034f0dd59bc08b0de
Андрей <viruszold@mail.ru>
2015-10-27 23:35:29 +0300

    Параметры для провайдера

--- Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/impl/ProviderImpl.java
+++ Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/impl/ProviderImpl.java
@@ -3,13 +3,17 @@ package ru.mirea.oop.practice.coursej.impl;
 import ru.mirea.oop.practice.coursej.api.Provider;
 
 import java.lang.reflect.Constructor;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class ProviderImpl<T> implements Provider<T> {
     private final Class<T> klass;
+    private final Object[] params;
     private volatile T created;
 
-    public ProviderImpl(Class<T> klass) {
+    public ProviderImpl(Class<T> klass, Object... objects) {
         this.klass = klass;
+        this.params = objects == null ? new Object[0] : objects;
     }
 
     @Override
@@ -18,9 +22,11 @@ public final class ProviderImpl<T> implements Provider<T> {
             synchronized (ProviderImpl.class) {
                 if (created == null) {
                     try {
-                        Constructor<T> constructor = klass.getDeclaredConstructor();
+                        Class[] classes = Stream.of(params).map(Object::getClass)
+                                .collect(Collectors.toList()).toArray(new Class[params.length]);
+                        Constructor<T> constructor = klass.getDeclaredConstructor(classes);
                         constructor.setAccessible(true);
-                        created = constructor.newInstance();
+                        created = constructor.newInstance(params);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
