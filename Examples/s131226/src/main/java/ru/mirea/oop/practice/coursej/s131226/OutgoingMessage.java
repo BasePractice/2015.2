dbf19dc6b2f972863e38f317526f81375b67d352
Stepan <cntgfy123@gmail.com>
2015-10-31 13:21:09 +0300

    Мелкие фиксы

--- Examples/s131226/src/main/java/ru/mirea/oop/practice/coursej/s131226/OutgoingMessage.java
+++ Examples/s131226/src/main/java/ru/mirea/oop/practice/coursej/s131226/OutgoingMessage.java
@@ -2,6 +2,8 @@ package ru.mirea.oop.practice.coursej.s131226;
 
 import com.google.gson.JsonParser;
 import com.squareup.okhttp.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.mirea.oop.practice.coursej.api.VkontakteApi;
 import ru.mirea.oop.practice.coursej.api.vk.DocumentsApi;
 import ru.mirea.oop.practice.coursej.api.vk.MessagesApi;
@@ -12,10 +14,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class OutgoingMessage {
+    private static final Logger logger = LoggerFactory.getLogger(OutgoingMessage.class);
     private long id;
     private String text;
     private String attachment;
     private final VkontakteApi api;
+    public static final int CHECK_COUNT = 10;
 
 
     public OutgoingMessage(long id, VkontakteApi api) {
@@ -35,15 +39,15 @@ public class OutgoingMessage {
         try {
             documentsApi = api.getDocuments();
 
-        Document[] documents = documentsApi.list(10, 0, 11105141);
+        Document[] documents = documentsApi.list(CHECK_COUNT, 0, api.idOwner());
         for (Document existDoc : documents) {
             if (existDoc.title .equals(file.getName())) {
-                if (documentsApi.delete(existDoc) == 1) System.out.println("успешно удален" + existDoc.toString());
+                if (documentsApi.delete(existDoc) == 1) logger.debug("успешно удален " + existDoc.title);
             }
         }
 
         documentsApi.uploadDocument(file);
-        documents=documentsApi.list(1, 0, 11105141);
+        documents=documentsApi.list(1, 0, api.idOwner());
         this.setAttachment("doc" + documents[0].idOwner + "_" + documents[0].id);
         } catch (Exception e) {
         e.printStackTrace();
