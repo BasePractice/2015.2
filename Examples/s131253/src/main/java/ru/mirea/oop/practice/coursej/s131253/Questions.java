6a436523032d3915bb24c4a3412046e597ec4125
Alex Yudin <Sasharus44@yandex.ru>
2015-12-18 02:42:50 +0300

    Добавление системы try-with-resources в класс Questions, файла с вопросами "Questions.txt" в resources, переименование основного класса VkBot в VkQuiz.

--- Examples/s131253/src/main/java/ru/mirea/oop/practice/coursej/s131253/Questions.java
+++ Examples/s131253/src/main/java/ru/mirea/oop/practice/coursej/s131253/Questions.java
@@ -28,28 +28,7 @@ public class Questions {
             e.printStackTrace();
         }
     }
- /*
-   BufferedReader reader = null;
-        try {  //Производится считывание вопросов для викторины из текстового файла
-            reader = new BufferedReader(new FileReader(new File(Configuration.getFileName("Questions.txt"))));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String line = null;
-        try {
-            assert reader != null;
-            while ((line = reader.readLine()) != null) {
-                if (line.contains("=")) {  //Каждая строка в документе разбивается символом "=" на 2 строковых элемента.
-                    String[] elements = line.split("=");
-                    baseOfQuestons.add(new Question(elements[0], elements[1])); */ /*Текст вопроса и ответ на него передаются
-                    в конструктор класса Question, а затем экземпляр класса добавляется в динамический массив */
- /*               }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-*/
+
     public ArrayList<Question> getBaseOfQuestons() {
         return baseOfQuestons;
     }
