93eaf3e20825b87251e6e31010a3af2d305d952e
Roman Zhigaikov <yaudav@yandex.ru>
2015-12-20 15:22:35 +0300

    Krasota + param file

--- Examples/s131237/src/main/java/ru/mirea/oop/practice/coursej/s131237/VkAudioStats.java
+++ Examples/s131237/src/main/java/ru/mirea/oop/practice/coursej/s131237/VkAudioStats.java
@@ -7,7 +7,6 @@ import com.google.gson.JsonParser;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.mirea.oop.practice.coursej.Configuration;
 import ru.mirea.oop.practice.coursej.api.vk.AudioApi;
 import ru.mirea.oop.practice.coursej.api.vk.FriendsApi;
 import ru.mirea.oop.practice.coursej.api.vk.UsersApi;
@@ -17,8 +16,6 @@ import ru.mirea.oop.practice.coursej.impl.vk.ext.ClientBotsExtension;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static java.lang.Thread.sleep;
@@ -53,7 +50,7 @@ public final class VkAudioStats extends ClientBotsExtension {
             "universities";
     private String modeChoice;
     private Integer tracksCount;
-    private String userId;
+    private String key;
 
     public VkAudioStats() throws Exception {
         super("vk.services.VkAudioStats");
@@ -67,25 +64,16 @@ public final class VkAudioStats extends ClientBotsExtension {
 
 
     public void loadParams() {
-        Properties props = new Properties();
-        InputStream is;
-
+        Properties prop = new Properties();
         try {
-            File f = new File("server.properties");
-            is = new FileInputStream( f );
+            prop.load(VkAudioStats.class.getResourceAsStream("/params"));
+        } catch (Exception ex) {
+            logger.info("Не удалось загрузить параметры");
         }
-        catch ( Exception e ) { is = null; }
 
-        try {
-            if ( is == null ) {
-                is = getClass().getResourceAsStream("server.properties");
-            }
-            props.load( is );
-        }
-        catch ( Exception e ) { }
-
-        modeChoice  = props.getProperty("modeChoice", "exact"); // all || exact
-        tracksCount = new Integer(props.getProperty("tracksCount", "30")); // maximum 8000
+        modeChoice = prop.getProperty("modeChoice"); // all || exact
+        tracksCount = new Integer(prop.getProperty("tracksCount")); // maximum 8000
+        key = prop.getProperty("key", "KLHMNCTQ90GNZUKBV");
     }
 
     @Override
@@ -99,11 +87,6 @@ public final class VkAudioStats extends ClientBotsExtension {
             UsersApi usersApi = api.getUsers();
             AudioApi audioApi = api.getAudios();
 
-
-            //Scanner s = new Scanner(System.in);
-
-            //logger.info("\nEnter \"all\" to handle all friends\nor \"exact\" to handle exact user\n");
-
             Contact[] contacts = new Contact[0]; //Получение массива пользователей
             switch (modeChoice) {
                 case "all":
@@ -116,31 +99,14 @@ public final class VkAudioStats extends ClientBotsExtension {
                     break;
             }
 
-            //int tracksCount = s.nextInt();
-
-
-            DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy_HHmmss");
-            Date date = new Date();
-
-
-            String path = "C:" + File.separator + "VkMusic" + File.separator + "log" + dateFormat.format(date) + ".txt";
-            File f = new File(path);
-            f.getParentFile().mkdirs();
-            try {
-                f.createNewFile();
-            } catch (IOException ex) {
-                logger.error("Ошибка создания файла", ex);
-            }
-
             //Проходим по всем контактам
             for (Contact contact : contacts) {
 
-                logger.info("\n=====================================================\n\n");
-                logger.info("обработка треков %s\n\n", Contact.viewerString(contact));
+                logger.info("=====================================================\n");
+                logger.info(String.format("обработка треков %s\n", Contact.viewerString(contact)));
 
                 friends.put(contact.id, contact);
 
-                URIBuilder builder = new URIBuilder();
                 JsonParser parser = new JsonParser();
 
                 //Запрос всех трэков пользователя
@@ -156,12 +122,12 @@ public final class VkAudioStats extends ClientBotsExtension {
                             //Получаем название группы
                             String artistName = track.artist;
 
-                            logger.info(artistName + " - " + track.title);
-                            builder = new URIBuilder();
+                            logger.info(String.format("%s - %s", artistName, track.title));
+                            URIBuilder builder = new URIBuilder();
 
                             //Использование api developer.echonest.com
                             builder.setScheme("http").setHost("developer.echonest.com").setPath("/api/v4/artist/profile")
-                                    .setParameter("api_key", "KLHMNCTQ90GNZUKBV") //ключ для усорения работы
+                                    .setParameter("api_key", key) //ключ для усорения работы api
                                     .setParameter("name", artistName)
                                     .setParameter("format", "json")
                                     .setParameter("bucket", "genre");
@@ -170,8 +136,7 @@ public final class VkAudioStats extends ClientBotsExtension {
                             try {
                                 JsonObject genreArrayObject = parser.parse(getJSON(builder.toString())).getAsJsonObject();
                                 genres = genreArrayObject.getAsJsonObject("response").getAsJsonObject("artist").getAsJsonArray("genres");
-                            }
-                            catch(Exception ex){
+                            } catch (Exception ex) {
                                 logger.error("Артист не найден");
                             }
 
@@ -206,6 +171,10 @@ public final class VkAudioStats extends ClientBotsExtension {
                         }
                     }
 
+                    logger.info("=====================================================\n");
+                    logger.info("Обработка трэков завершена\n");
+                    logger.info("=====================================================\n");
+                    logger.info("Статистика:\n");
 
                     for (Map.Entry<String, Float> entry : audioStats.entrySet()) {
                         if (sum != 0) {
@@ -219,7 +188,7 @@ public final class VkAudioStats extends ClientBotsExtension {
 
                     //Вывод статистики в файл
                     for (Map.Entry<String, Float> entry : genreList) {
-                        logger.info(String.format("%s - %.2f%%\n", entry.getKey(), entry.getValue()));
+                        logger.info(String.format("%s - %.2f%%", entry.getKey(), entry.getValue()));
                     }
                 } else {
                     logger.info("Аудиозаписи пользователя недоступны");
@@ -245,23 +214,23 @@ public final class VkAudioStats extends ClientBotsExtension {
 
             int resp = con.getResponseCode();
             if (resp == 200) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"))) {
+                    String line;
+                    StringBuilder sb = new StringBuilder();
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                    }
+                    br.close();
 
-                String line;
-                StringBuilder sb = new StringBuilder();
-                while ((line = br.readLine()) != null) {
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
+                    return sb.toString();
                 }
-                br.close();
-
-                return sb.toString();
             } else {
                 logger.info("RESP", "Ответ сервера: " + resp);
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Ошибка получения JSON");
         }
 
         return null;
