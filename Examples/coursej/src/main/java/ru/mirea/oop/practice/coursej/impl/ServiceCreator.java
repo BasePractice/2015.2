203184231b4110635fcf346de293bd6da712bf6e
aleksejpluhin <pk-feso@mail.ru>
2015-12-22 09:32:48 +0300

    rvert "ООП"
    This reverts commit 4b143375b7464f04109f0a7d9af0bd60d0081820.

--- Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/impl/ServiceCreator.java
+++ Examples/coursej/src/main/java/ru/mirea/oop/practice/coursej/impl/ServiceCreator.java
@@ -4,11 +4,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import retrofit.Retrofit;
+import ru.mirea.oop.practice.coursej.api.vk.ArrayDeserializer;
+import ru.mirea.oop.practice.coursej.api.vk.entities.Audio;
 import ru.mirea.oop.practice.coursej.api.vk.entities.Document;
 
 public final class ServiceCreator {
     public static final Gson gson = new GsonBuilder()
-            .registerTypeAdapter(Document[].class, new Document.ArrayDeserializer())
+            .registerTypeAdapter(Document[].class, new ArrayDeserializer<>(Document.class, Document[]::new))
+            .registerTypeAdapter(Audio[].class, new ArrayDeserializer<>(Audio.class, Audio[]::new))
             .setPrettyPrinting().create();
 
     private ServiceCreator() {
