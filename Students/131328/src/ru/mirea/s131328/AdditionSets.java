8b2b1fc975a1037ab1d5df58ff37a452abadcdb5
Olya Belyaeva <nightofthewhisper@gmail.com>
2015-12-23 09:32:32 +0300

    Пересечение множеств

---
+++ Students/131328/src/ru/mirea/s131328/AdditionSets.java
@@ -0,0 +1,26 @@
+package ru.mirea.s131328;
+
+import java.util.Set;
+import java.util.TreeSet;
+/**
+ * Пересечение множеств
+*/
+public class AdditionSets {
+    public static void main(String[] args) {
+        int[] array1 = {0, 1, 2, 3, 4, 5, 8, 999};
+        int[] array2 = {1, 2, 3, 4, 5, 12};
+        Set<Integer> result = new TreeSet<>();
+
+        for (int a : array1) {
+            result.add(a);
+        }
+        for (int a : array2) {
+            result.add(a);
+        }
+
+
+        for (int a : result) {
+            System.out.println(a);
+        }
+    }
+}
