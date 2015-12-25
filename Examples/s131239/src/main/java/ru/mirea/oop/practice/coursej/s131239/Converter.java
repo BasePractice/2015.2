8bdbb8eea0c690149ac4fd8c9781891dedaf00d7
imnothumanimzombie <ahscoven982@gmail.com>
2015-12-25 19:03:50 +0300

    Converter

--- Examples/s131239/src/main/java/ru/mirea/oop/practice/coursej/s131239/Converter.java
+++ Examples/s131239/src/main/java/ru/mirea/oop/practice/coursej/s131239/Converter.java
@@ -8,11 +8,10 @@ import ru.mirea.oop.practice.coursej.impl.vk.ext.ServiceBotsExtension;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * -3?
- */
 public final class Converter extends ServiceBotsExtension {
     private static final Logger logger = LoggerFactory.getLogger(Converter.class);
+    HashMap<String, HashMap<String, Double>> map;
+    private boolean isMapLoaded = false;
 
     public Converter() throws Exception {
         super("vk.services.Converter");
@@ -25,53 +24,46 @@ public final class Converter extends ServiceBotsExtension {
                 Message msg = (Message) event.object;
                 Contact contact = msg.contact;
                 if (msg.isOutbox()) {
-                    logger.debug("Сообщение для " + Contact.viewerString(contact)
-                            + ", не следует на него отвечать оно исходящее");
-                    logger.debug("Текст сообщения: " + msg.text);
+                    logger.debug(MessageFormat.format("Сообщение для {0}, не следует на него отвечать, оно исходящее", Contact.viewerString(contact)));
+                    logger.debug(MessageFormat.format("Текст сообщения: {0}", msg.text));
                     break;
                 }
-                logger.debug("Получили сообщение от " + Contact.viewerString(contact));
+                logger.debug(MessageFormat.format("Получили сообщение от: {0}", Contact.viewerString(contact)));
+
+                if (!isMapLoaded) {
+                    map = new HashMap<String, HashMap<String, Double>>();
+                    BufferedReader reader;
+                    String line;
+                    try {
+                        reader = new BufferedReader(new FileReader(new File(Configuration.getFileName("values.txt"))));
+                        while ((line = reader.readLine()) != null) {
+                            if (line.contains("/")) {
+                                String[] strings = line.split("/");
+                                if (map.containsKey(strings[0])) {
+                                    map.get(strings[0]).put(strings[1], Double.parseDouble(strings[2]));
+                                } else {
+                                    map.put(strings[0], new HashMap<String, Double>());
+                                    map.get(strings[0]).put(strings[1], Double.parseDouble(strings[2]));
+                                }
+                            }
+                        }
+                    } catch (FileNotFoundException e) {
+                        logger.error("Не найден файл с базой конвертаций: ", e);
+                    } catch (IOException e) {
+                        logger.error("Ошибка чтения файла с базой конвертаций: ", e);
+                    }
+                    isMapLoaded = true;
+                }
 
                 if (msg.text.split(" ")[0].matches("[-+]?\\d+") && msg.text.split(" ").length == 2) {
                     int num = Integer.parseInt(msg.text.split(" ")[0]);
                     String predesc = msg.text.split(" ")[1];
-                    HashMap<String, HashMap<String, Double>> map = new HashMap<String, HashMap<String, Double>>();
                     String desc = predesc.replace("сантиметры", "см").replace("сантиметров", "см").
                             replace("сантиметра", "см").replace("сантиметр", "см").replace("баров", "бар").
-                            replace("бары", "бар");
-                    map.put("см", new HashMap<String, Double>());
-                    map.get("см").put("мм", 10.0);
-                    map.get("см").put("м", 0.01);
-                    map.get("см").put("дм", 0.1);
-                    map.get("см").put("км", 0.00001);
-                    map.get("см").put("фут", 0.0328084);
-                    map.get("см").put("микрон", 10000.0);
-                    map.get("см").put("нм", 10000000.0);
-                    map.get("см").put("A", 100000000.0);
-                    map.get("см").put("верста", 0.00000937382827);
-                    map.get("см").put("межевая верста", 0.000004687);
-                    map.get("см").put("косая сажень", 0.004032);
-                    map.get("см").put("сажень", 0.004687);
-                    map.get("см").put("маховая сажень", 0.005682);
-                    map.get("см").put("аршин", 0.01406);
-                    map.get("см").put("локоть", 0.02381);
-                    map.get("см").put("пядь", 0.05624);
-                    map.get("см").put("вершок", 0.225);
-                    map.get("см").put("дюйм", 0.3937);
-                    map.get("см").put("линия", 3.937);
+                            replace("бары", "бар").replace("фута", "фут").replace("футов", "фут").
+                            replace("микрона", "микрон").replace("микронов", "микрон").replace("дюйма", "дюйм").
+                            replace("дюймов", "дюйм").replace("паскаля", "паскаль").replace("паскалей", "паскаль");
 
-
-                    map.put("бар", new HashMap<String, Double>());
-                    map.get("бар").put("паскаль", 100000.0);
-                    map.get("бар").put("кПа", 100.0);
-                    map.get("бар").put("гПа", 1000.0);
-                    map.get("бар").put("МПа", 0.1);
-                    map.get("бар").put("атм", 0.9869);
-                    map.get("бар").put("милибар", 1000.0);
-                    map.get("бар").put("торр", 750.1);
-                    map.get("бар").put("psi", 14.5);
-                    map.get("бар").put("ksi", 0.0145);
-                    map.get("бар").put("osi", 232.1);
                     StringBuilder msgToSend = new StringBuilder();
                     if (map.containsKey(desc)) {
                         for (Map.Entry<String, Double> stringDoubleEntry : map.get(desc).entrySet()) {
@@ -91,6 +83,6 @@ public final class Converter extends ServiceBotsExtension {
 
     @Override
     public String description() {
-        return "Сервис пересылки сообщения \"Вконтакте\"";
+        return "Сервис конвертации величин \"Вконтакте\"";
     }
 }
